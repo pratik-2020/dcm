e12734e21a475cb2f623fa3d6f44b8ed80a9629c
Pratik Singh Thakur <pratikthakur2019@gmail.com>
2023-01-24 21:06:53 +0530

    added / route in server.js

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(cors({
     ]
 }))
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Welcome');
+})
 app.post('/send', (req, res) => {
     // sendMail(req, res);
     const name = req.body.name;
